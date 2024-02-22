72332110564332fbc4c5c2ade525f742f82b7983
NicolaBellini <bellininicola4366@gmail.com>
2024-02-22 15:53:36 +0100

    [add] an input and a button

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,46 @@
-const container = document.getElementById("canva")
+ 
+document.addEventListener("DOMContentLoaded", function(){
+ const container = document.getElementById("canva")
+ 
+  let btn = document.getElementById("btn")
 
 
-const limit = 101;
+  // const limit = 100;
+  btn.addEventListener("click", create)
 
-for(i = 1; i < limit; i++){
-  if((!(i % 3) && (!(i % 5)))){
-    console.log(i +" multiplo sia di 3 che di 5");
-    container.innerHTML += `
-    <div class="box multiple-3-5">fizzbuzz</div>
-    `
-  }else if(!(i % 5)){
-    console.log(i + " multiplo di 5");
-    container.innerHTML += `
-    <div class="box multiple-5">buzz</div>
-    `
-  }else if  (!(i % 3)){
-    console.log(i + " multiplo di 3");
-    container.innerHTML += `
-    <div class="box multiple-3">fizz</div>
-    `
-  }else{
-    console.log(i);
-    container.innerHTML += `
-    <div class="box normal">${i}</div>
-    `
-  }
-} 
-  // console.log(i);
+  function create(){
+    let inputNum = document.getElementById("num").value
+    let limit = inputNum;
+    console.log(inputNum, limit);
+   
+    
+    container.innerHTML = ""
+
+    for(i = 1; i < limit; i++){
+      if((!(i % 3) && (!(i % 5)))){
+        console.log(i +" multiplo sia di 3 che di 5");
+        container.innerHTML += `
+        <div class="box multiple-3-5">fizzbuzz</div>
+        `
+      }else if(!(i % 5)){
+        console.log(i + " multiplo di 5");
+        container.innerHTML += `
+        <div class="box multiple-5">buzz</div>
+        `
+      }else if  (!(i % 3)){
+        console.log(i + " multiplo di 3");
+        container.innerHTML += `
+        <div class="box multiple-3">fizz</div>
+        `
+      }else{
+        console.log(i);
+        container.innerHTML += `
+        <div class="box normal">${i}</div>
+        `
+      }
+    } 
+  } 
+
+
+});
+
