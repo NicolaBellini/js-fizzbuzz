fb2e8dd12e87407f4e21737ab12bfe653d4c60d1
NicolaBellini <bellininicola4366@gmail.com>
2024-02-22 16:07:07 +0100

    [add] placeholder and alert

--- JS/script.js
+++ JS/script.js
@@ -8,39 +8,42 @@ document.addEventListener("DOMContentLoaded", function(){
   // const limit = 100;
   btn.addEventListener("click", create)
 
-  function create(){
-    let inputNum = document.getElementById("num").value
-    let limit = inputNum;
-    console.log(inputNum, limit);
-   
+    function create(){
+      let inputNum = document.getElementById("num").value
+      let limit = inputNum;
+      console.log(inputNum, limit);
     
-    container.innerHTML = ""
-
-    for(i = 1; i < limit; i++){
-      if((!(i % 3) && (!(i % 5)))){
-        console.log(i +" multiplo sia di 3 che di 5");
-        container.innerHTML += `
-        <div class="box multiple-3-5">fizzbuzz</div>
-        `
-      }else if(!(i % 5)){
-        console.log(i + " multiplo di 5");
-        container.innerHTML += `
-        <div class="box multiple-5">buzz</div>
-        `
-      }else if  (!(i % 3)){
-        console.log(i + " multiplo di 3");
-        container.innerHTML += `
-        <div class="box multiple-3">fizz</div>
-        `
+      
+      container.innerHTML = ""
+      if(!(isNaN(inputNum))){
+        for(i = 1; i < limit; i++){
+          if((!(i % 3) && (!(i % 5)))){
+            console.log(i +" multiplo sia di 3 che di 5");
+            container.innerHTML += `
+            <div class="box multiple-3-5">fizzbuzz</div>
+            `
+          }else if(!(i % 5)){
+            console.log(i + " multiplo di 5");
+            container.innerHTML += `
+            <div class="box multiple-5">buzz</div>
+            `
+          }else if  (!(i % 3)){
+            console.log(i + " multiplo di 3");
+            container.innerHTML += `
+            <div class="box multiple-3">fizz</div>
+            `
+          }else{
+            console.log(i);
+            container.innerHTML += `
+            <div class="box normal">${i}</div>
+            `
+          }
+        } 
       }else{
-        console.log(i);
-        container.innerHTML += `
-        <div class="box normal">${i}</div>
-        `
+        alert(" BRUHH! Inserisci un numero e vedi che funziona.")
       }
     } 
-  } 
 
 
-});
+  });
 
