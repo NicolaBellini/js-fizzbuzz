33d2f3739d8b80aa0ca66bc50e735c341be6874e
NicolaBellini <bellininicola4366@gmail.com>
2024-02-22 14:42:16 +0100

    [fix] for loop

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,21 @@
+const container = document.getElementById("canva")
+
+
 const limit = 101;
 
 for(i = 1; i < limit; i++){
-  if(!(i % 3)){
-    console.log(i + " multiplo di 3")
+  if((!(i % 3) && (!(i % 5)))){
+    console.log(i +" multiplo sia di 3 che di 5");
+
   }else if(!(i % 5)){
     console.log(i + " multiplo di 5");
+
+  }else if  (!(i % 3)){
+    console.log(i + " multiplo di 3");
+
   }else{
     console.log(i);
+
   }
 } 
   // console.log(i);
-
-console.log("ciao");
