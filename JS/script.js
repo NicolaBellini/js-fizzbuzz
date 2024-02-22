bb5a5ae77f4b3d006d7deb9367c80f04a4e6342c
NicolaBellini <bellininicola4366@gmail.com>
2024-02-22 14:33:05 +0100

    [add] for loop

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const limit = 101;
+
+for(i = 1; i < limit; i++){
+  if(!(i % 3)){
+    console.log(i + " multiplo di 3")
+  }else if(!(i % 5)){
+    console.log(i + " multiplo di 5");
+  }else{
+    console.log(i);
+  }
+} 
+  // console.log(i);
+
+console.log("ciao");
