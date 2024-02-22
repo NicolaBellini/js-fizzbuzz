38ea64949f31761488d333133f76759af6ff1f5e
NicolaBellini <bellininicola4366@gmail.com>
2024-02-22 14:51:10 +0100

    [add] css classes and added innerHTML

--- JS/script.js
+++ JS/script.js
@@ -6,16 +6,24 @@ const limit = 101;
 for(i = 1; i < limit; i++){
   if((!(i % 3) && (!(i % 5)))){
     console.log(i +" multiplo sia di 3 che di 5");
-
+    container.innerHTML += `
+    <div class="box multiple-3-5">${i}</div>
+    `
   }else if(!(i % 5)){
     console.log(i + " multiplo di 5");
-
+    container.innerHTML += `
+    <div class="box multiple-5">${i}</div>
+    `
   }else if  (!(i % 3)){
     console.log(i + " multiplo di 3");
-
+    container.innerHTML += `
+    <div class="box multiple-3">${i}</div>
+    `
   }else{
     console.log(i);
-
+    container.innerHTML += `
+    <div class="box normal">${i}</div>
+    `
   }
 } 
   // console.log(i);
